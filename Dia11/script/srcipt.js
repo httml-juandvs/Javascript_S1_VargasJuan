27b96b766531f47cae777e77b0c724e53f4ea6bd
httml-juandvs <juandavargassoto@gmail.com>
2025-05-05 11:41:34 +0000

    fix: :bug: Edit srcipt.js

--- Dia11/script/srcipt.js
+++ Dia11/script/srcipt.js
@@ -1,5 +1,25 @@
+async function api() {
+    let response = await fetch ("https://67f91c1b094de2fe6ea07f34.mockapi.io/productos/1");
+    let data = await response.json();
 
-
-
-    
+    return data;
 }
+
+async function d1() {
+    let data = await api();
+
+    let producto = document.getElementById("nombre");
+    let foto = document.getElementById("imagen");
+    let precio = document.getElementById("precio");
+    let categoria = document.getElementById("tipo");
+    let stock = document.getElementById("inventario");
+
+    producto.textContent = data ["name"];
+    foto.src = data ["image"];
+    precio.textContent= data["price"];
+    categoria.textContent = data ["category"];
+    stock.textContent = data ["stock"]
+
+   
+}
+d1()
